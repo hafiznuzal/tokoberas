a2fdb987995821e0017315665c71bbaad6b9b4b8
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2016-12-08 05:56:35 +0700

    Create empty migrations

---
+++ database/migrations/2016_12_07_224502_create_inventory_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInventoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventory', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama');
+            $table->string('merek');
+            $table->integer('jenis_id')->unsigned();
+            $table->integer('modal_id')->unsigned();
+            $table->bigInteger('harga_beli');
+            $table->date('tanggal_masuk');
+            $table->date('tanggal_kadaluarsa');
+            $table->integer('jumlah');
+            $table->integer('jumlah_aktual');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventory');
+    }
+}
