df50ab97c072c6cf657c63c95dcf6e22b3005f06
Hafiz Nuzal Djufri <hafiz.djufri@gmail.com>
2016-12-12 17:39:51 +0700

    edit laporan pembayaran

--- resources/views/app/tabel_laporan_pembayaran.blade.php
+++ resources/views/app/tabel_laporan_pembayaran.blade.php
@@ -11,6 +11,73 @@
 @endsection
 
 @section('content')
+<div class="row">
+  <div class="col-md-12 col-sm-12 col-xs-12">
+    <div class="box">
+      <div class="box-header box-body">
+        <h3>Tambah Konsumen</h3>  
+      </div>
+      <div class="box box-primary">
+      <div class="box-header with-border">
+       
+        <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left" method="post" action="{{url('konsumen')}}">
+          {{csrf_field()}}
+          <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Nama<span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <input type="text" id="first-name" name="nama" required="required" class="form-control col-md-7 col-xs-12">
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Tanggal lahir<span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <div class="input-group date">
+                  <div class="input-group-addon">
+                    <i class="fa fa-calendar"></i>
+                  </div>
+                  <input type="date" class="form-control pull-right" name="tanggallahir">
+                </div>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Alamat <span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <input type="text" id="last-name" name="alamat" required="required" class="form-control col-md-7 col-xs-12">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">nomor telepon</label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <input id="middle-name" class="form-control col-md-7 col-xs-12" type="text" name="telepon">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">nomor handphone <span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <input type="text" id="first-name" name="hp" required="required" class="form-control col-md-7 col-xs-12">
+            </div>
+          </div>
+          <div class="ln_solid"></div>
+          <div class="form-group">
+            <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+              <button type="submit" class="btn btn-primary">Cancel</button>
+              <button type="submit" class="btn btn-success">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
 <div class="col-md-12 col-sm-12 col-xs-12">
   <div class="box">
     <div class="box-header box-body">
@@ -44,8 +111,10 @@
     </table>
   </div>
   </div>
+  </div>
 </div>
 </div>
+
 
 @endsection
 
